5ba36a0c0c48c09b772e2609ae14fc3527157d26
UlkaIgn <misulia81@gmail.com>
2025-10-19 17:19:54 +0500

    исправ теста1

--- src/test/java/WikipediaTest.java
+++ src/test/java/WikipediaTest.java
@@ -1,4 +1,3 @@
-pimport com.codeborne.selenide.logevents.SelenideLogger;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
